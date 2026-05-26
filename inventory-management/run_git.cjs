3d8e8865af165575907fa643f43c74854e32fc81
kimiepantoja14-cmd <kimiepantoja14@gmail.com>
2026-05-26 08:21:47 +0800

    Delete inventory-management directory

--- inventory-management/run_git.cjs
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  const output = execSync('git status', { encoding: 'utf8' });
-  console.log('Git Status:\n', output);
-} catch (err) {
-  console.error('Git error:', err.message);
-  if (err.stdout) console.log('Stdout:', err.stdout.toString());
-  if (err.stderr) console.error('Stderr:', err.stderr.toString());
-}
